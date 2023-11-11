02e7905d710423897eec5594fa423495af1a6d8b
Arktetra <sameershrestha.sm@gmail.com>
2023-11-11 11:45:09 +0545

    feat(calculator): add trigonometric operations

--- MVIz/Math/Calculator/test/test.cpp
+++ MVIz/Math/Calculator/test/test.cpp
@@ -1,9 +1,15 @@
 #include "../lexer.cpp"
 
 int main() {
-    std::string text = "2 * 2 + (3 * 4)";
+    std::string text = "sin(30)";
 
     Lexer l(text);
+    std::cout << l.GetNextToken().GetToken() << std::endl;
+    std::cout << l.GetNextToken().GetToken() << std::endl;
+    std::cout << l.GetNextToken().GetToken() << std::endl;
+    std::cout << l.GetNextToken().GetToken() << std::endl;
+    std::cout << l.GetNextToken().GetToken() << std::endl;
+    std::cout << l.GetNextToken().GetToken() << std::endl;
     std::cout << l.GetNextToken().GetToken() << std::endl;
     std::cout << l.GetNextToken().GetToken() << std::endl;
     std::cout << l.GetNextToken().GetToken() << std::endl;
