ab2a76e5f44d8a0981e428ccc78af205ef39ccd3
Arktetra <sameershrestha.sm@gmail.com>
2023-11-19 16:57:52 +0545

    feat(calculator): add ranged operations
    Calculator can now solve expressions like:
        -(x^2) + x + 1
    and return a vector containing result for the range provided in the
    function `calculate`.

--- MVIz/Math/Calculator/calculator.cpp
+++ MVIz/Math/Calculator/calculator.cpp
@@ -3,8 +3,10 @@
 #include "lexer.cpp"
 #include "../../logger.h"
 #include <iostream>
+#include <sstream>
+#include <vector>
 
-float Calculator(std::string text) {
+float Calculate(std::string text) {
     Logger::Log("Initializing Calculator\n", Severity::Info);
     std::string message;
     message = "Calculator Data\n\n\tInput Expression: " + text;
@@ -17,4 +19,46 @@ float Calculator(std::string text) {
     message = message + "\n\tOutput: " + std::to_string(interpreter.Result()) + "\n";
     Logger::Log(message, Severity::Info);
     return interpreter.Result();
+}
+
+std::vector<float> Calculate(std::string text, float low, float high, float step_size = 1) {
+    Logger::Log("Initializing Calculator\n", Severity::Info);
+    std::ostringstream oss;
+    oss << "Calculator Data\n\n\tInput Expression: " << text;
+    oss << "\n\n\tVertices:";
+    oss << std::setw(6) << "X " << "\t\t\t\tY\n";
+
+    std::string original_text = text;
+
+    std::vector<float> result;
+
+
+    for (float i = low; i <=  high; i += step_size) {
+        oss << "\n\t" << std::setw(15) << i << "\t\t";
+
+        for (int j = 0; j < text.length(); j++) {
+            if (text[j] == 'x') {
+                text.erase(j, 1);
+                text.insert(j, std::to_string(i));
+            }
+        }
+
+        Lexer lexer(text);
+        Parser parser(&lexer);
+        auto ast = parser.Expression();
+
+        Interpreter interpreter;
+        ast.get()->accept(&interpreter);
+        oss << std::setw(10) << std::setprecision(3) << interpreter.Result();
+
+        result.push_back(interpreter.Result());
+
+        text = original_text;
+    }
+
+    oss << "\n";
+
+    Logger::Log(oss.str(), Severity::Info);
+
+    return result;
 }
